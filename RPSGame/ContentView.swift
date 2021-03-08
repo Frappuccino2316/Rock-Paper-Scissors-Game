b683c8aeab093a6ff4218d11a7c7ce21626b4d5d
Frappuccino001 <fumi00hiro@icloud.com>
2021-03-08 23:17:26 +0900

    Initial Commit

---
+++ RPSGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RPSGame
+//
+//  Created by 髙木文弘 on 2021/03/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RPSGame/RPSGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSGameApp.swift
+//  RPSGame
+//
+//  Created by 髙木文弘 on 2021/03/08.
+//
+
+import SwiftUI
+
+@main
+struct RPSGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
